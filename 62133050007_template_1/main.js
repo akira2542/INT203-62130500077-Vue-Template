cef19fee365b5713c985a94784e74b9293846399
Ruangrit <akiraakamura@gmail.com>
2021-02-08 10:24:43 +0700

    replace template from template repo

--- 62133050007_template_1/main.js
+++ 62133050007_template_1/main.js
@@ -1,9 +1,9 @@
-const app = {
-    data () {
-        return {
-            msg : "Hello Vue Js"
+    const app = {
+        data() {
+            return {
+                msg: 'Hello, Vue3'
+            }
         }
-    }
-}
 
-Vue.createApp(app).mount('#app')
+    }
+    Vue.createApp(app).mount('#app')
